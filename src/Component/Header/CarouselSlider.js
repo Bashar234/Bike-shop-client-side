bdd7f4ed177eb8984688ac9aadf73b475cd2d948
unknown <you@example.com>
2022-12-02 19:05:30 +0600

    header part done

--- src/Component/Header/CarouselSlider.js
+++ src/Component/Header/CarouselSlider.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 const CarouselSlider = () => {
   return (
-    <Carousel>
+    
       <div className="CaroImg">
         <img
           className="img-fluid"
